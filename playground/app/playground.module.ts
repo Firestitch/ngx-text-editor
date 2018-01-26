1f86dd02c3ef91baf887e34ca9dcf1c9b26b7699
Raymond <ray@firestitch.com>
2018-01-26 07:31:00 -0500

    initial commit

---
+++ playground/app/playground.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FsExampleModule } from '@firestitch/example';
+import { FsMessageModule } from '@firestitch/message';
+
+import { ToastrModule } from 'ngx-toastr';
+
+import { FsComponentModule } from '@firestitch/package';
+
+import { AppMaterialModule } from './material.module';
+import {
+  ExampleComponent,
+  ExamplesComponent
+} from './components';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: ExamplesComponent },
+];
+
+@NgModule({
+  bootstrap: [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FsComponentModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    FormsModule,
+    FsExampleModule.forRoot(),
+    FsMessageModule.forRoot(),
+    ToastrModule.forRoot({ preventDuplicates: true }),
+    RouterModule.forRoot(routes),
+  ],
+  entryComponents: [
+  ],
+  declarations: [
+    AppComponent,
+    ExamplesComponent,
+    ExampleComponent
+  ],
+  providers: [
+  ],
+})
+export class PlaygroundModule {
+}
---
+++ src/app/components/component/component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fs-component',
+  templateUrl: 'component.component.html',
+  styleUrls: [ 'component.component.scss' ],
+})
+export class FsComponentComponent {
+
+  constructor() {
+  }
+}
